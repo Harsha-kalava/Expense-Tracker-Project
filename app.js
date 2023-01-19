15675d0437c6e79aa5abdf470d4e09fbc25ea162
Harsha-kalava <harshcode16@gmail.com>
2023-01-19 20:39:14 +0530

    S3 checking

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ resetPassword.belongsTo(User)
 User.hasMany(fileData)
 fileData.belongsTo(User)
 
-sequelize.sync({alter:true})
+sequelize.sync({force:true})
 .then((res)=>{
     app.listen(3000,()=>{
         console.log('app started running on 3000')
